e773b34e91fce44093b711353b90d91396b37ca3
railee091 <74151748+railee091@users.noreply.github.com>
2021-10-03 21:45:36 +0800

    x

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+</html>
